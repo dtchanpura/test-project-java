23313bc32d9bf1cd65614077a8a451f501c75359
Darshil Chanpura <darshil.c@iqm.com>
2018-09-03 10:57:05 +0530

    Update: added code and pom.xml

---
+++ src/main/java/Application.java
@@ -0,0 +1,27 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+    private static Integer counter = 0;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        counter++;
+        try {
+            // Emulate business logic
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Hello World: " + String.valueOf(counter);
+    }
+
+}
